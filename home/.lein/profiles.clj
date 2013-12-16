11e7f7a492eb04abe38413b9fc317cf963aebd11
Bryan Gahagan <bgahagan@gmail.com>
2013-12-15 20:24:58 -0500

    latest dotfiles

---
+++ home/.lein/profiles.clj
@@ -0,0 +1,5 @@
+{:user {:plugins [[lein-kibit "0.0.8"]
+                  [lein-deps-tree "0.1.2"]
+                  [lein-try "0.2.0"]
+                  [lein-autoreload "0.1.0"]
+                  [lein-bikeshed "0.1.3"]]}}
